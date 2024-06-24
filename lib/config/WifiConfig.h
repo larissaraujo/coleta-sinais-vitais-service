e53034e769c2c48079a21b465118888b008f9a5b
larissaraujo <55551357+larissaraujo@users.noreply.github.com>
2024-06-24 18:52:33 -0300

    Update WifiConfig.h

--- lib/config/WifiConfig.h
+++ lib/config/WifiConfig.h
@@ -5,8 +5,8 @@
 #include <HTTPClient.h>
 #include <WiFi.h>
 
-#define WIFI_SSID "Aguinaldo" // rede wifi
-#define WIFI_PASSWORD "dinho281" // senha da rede wifi
+#define WIFI_SSID "" // rede wifi
+#define WIFI_PASSWORD "" // senha da rede wifi
 #define WIFI_TIMEOUT_MS 20000
 
 void connectWifi();
